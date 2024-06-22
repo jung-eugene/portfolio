2fdd140f31c95ed966463ab75b720e4980360ff3
Eugene Jung <eugenejung04@gmail.com>
2024-06-22 19:30:35 -0400

    set up project page with cards

--- pages/project.jsx
+++ pages/project.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import styles from "../styles/Project.module.css";
 
-const project = () => {
-  return <div>project</div>;
+const projects = [
+  {
+    title: "Project 1",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    commits: "4,245 commits",
+  },
+  {
+    title: "Project 2",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    commits: "245 commits",
+  },
+];
+
+const Project = () => {
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.heading}>// Top projects</h2>
+      <div className={styles.grid}>
+        {projects.map((project, index) => (
+          <div key={index} className={styles.card}>
+            <div className={styles.cardHeader}>
+              <h3 className={styles.title}>{project.title}</h3>
+              <span className={styles.commits}>{project.commits}</span>
+            </div>
+            <div className={styles.codeBlock}>
+              <pre>
+                <code>
+                  {`/**\n * ${project.title}\n * ${project.description}\n */`}
+                </code>
+              </pre>
+            </div>
+            <button className={styles.button}>see the project</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default project;
+export default Project;
